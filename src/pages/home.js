91634fc3f2854b41ffa6b9ddfb055945eeaa36eb
Sheeba Sharon <sheebasharon@Sheebas-MacBook-Air.local>
2024-12-04 18:47:25 +0530

    created the card

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,8 +1,16 @@
 import React from 'react';
-
+import "./home.css"
 function Home() {
-    return( <div>
-      <h1>this is home</h1>
+    return( <div className="home-container">
+      <h1 className="home-heading">
+        Unlock the secrets of your dreams and track your mood journey—understand yourself better, one night and one day at a time
+         </h1>
+      <div class="card">
+         <div class="card-body">
+         Tap the card, let magic play—unlock a quote to brighten your day!
+         </div>
+         <button>Click me</button>
+      </div>
     </div>);
 }
 
